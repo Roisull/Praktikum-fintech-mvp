ff26dc8f9d13c13be1b2445f2a8ec7fc8f2a756d
roisul <93356107+Roisull@users.noreply.github.com>
2022-06-07 11:32:16 +0700

    Create MainPresenter.kt

---
+++ MainPresenter.kt
@@ -0,0 +1,31 @@
+package com.example.mvp
+
+class MainPresenter(private val mainView: MainView) {
+    fun hitungLuasPersegiPanjang(panjang: Float, lebar: Float){
+        if(panjang == 0F || panjang == null){
+            mainView.showError("Panjang tidak boleh 0")
+            return
+        }
+        if(lebar == 0F || lebar == null){
+            mainView.showError("Lebar tidak boleh 0")
+            return
+        }
+
+        val luas = panjang * lebar
+        mainView.updateLuas(luas)
+    }
+
+    fun hitungKelilingPersegiPanjang(panjang: Float, lebar: Float){
+        if(panjang == 0F || panjang == null){
+            mainView.showError("Panjang tidak boleh 0")
+            return
+        }
+        if(lebar == 0F || lebar == null){
+            mainView.showError("Lebar tidak boleh 0")
+            return
+        }
+
+        val keliling = 2*(panjang+lebar)
+        mainView.updateKeliling(keliling)
+    }
+}
